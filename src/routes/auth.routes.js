4f868115c6e5bce893cb268ca5a437e8ca1a5bc5
Zabala Gustavo <capricorniano1969@gmail.com>
2021-08-21 15:27:28 -0300

    cambio de registrarse por register

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 const authRouter = require("express").Router();
-const { login , registrarse } = require("../controller/auth.controller");
+const { login , register } = require("../controller/auth.controller");
 
 authRouter.post("/login", login);
-authRouter.post("/registrarse", registrarse);
+authRouter.post("/register", register);
 
 module.exports = authRouter;
