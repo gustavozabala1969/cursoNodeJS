347e26c71f0ef71286d1617b46b5a61bfbcda00e
Zabala Gustavo <capricorniano1969@gmail.com>
2021-08-22 16:09:14 -0300

    DB, PASS, NAME de DB de prod

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -25,9 +25,13 @@ module.exports = {
     dialect: "postgres"
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
+    //username: "root",
+    //password: null,
+    //database: "database_production",
+    //host: "127.0.0.1",
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
     host: "127.0.0.1",
     dialect: "postgres",
     dialectOptions: {
